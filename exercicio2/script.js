00a26127724c2bd4323400c76f466f4917d6fcbb
Raphael Valerio <raphaelsvalerio@hotmail.com>
2022-08-03 23:00:10 -0300

    Exercicios Resolvidos A012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,19 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade=18;
+let fimEnsMed =true;
+let cursandoFac =false;
 
-if(){
-
+if(idade >= 18){
+    console.log("Tem 18 anos ou mais.")
 }else{
-	
+    console.log("Não tem 18 anos ou mais.")	
 }
+if(fimEnsMed === true){
+    console.log("Terminou o Ensino Médio.")
+}else{
+    console.log("Não terminou o Ensino Médio.")
+}
+if(cursandoFac === false){
+    console.log("Não está cursando nenhuma faculdade, apenas o curso da Labenu!!")
+}else{
+    console.log("Está cursando uma faculdade.")
+};
